4d2d320e403b5460e9c515332bc2bb927e23c944
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2026-05-25 12:11:34 +0800

    fix: update Indonesian validation messages for consistency and clarity

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -29,8 +29,7 @@
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link
-    href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
+  <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
     rel="stylesheet">
 
   @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -41,13 +40,11 @@
 <body class="h-full">
   {{ $slot }}
 
-  {{-- PWA install banner --}}
   <x-pwa-install-banner />
 
   @livewireScripts
   @stack('scripts')
 
-  {{-- Paksa update service worker + hapus semua cache lama --}}
   <script>
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.getRegistrations().then(function(regs) {
@@ -63,7 +60,6 @@
     }
   </script>
 
-  {{-- Pastikan browser tab title ter-update saat wire:navigate --}}
   <script data-navigate-once>
     document.addEventListener('livewire:navigated', () => {
       const titleEl = document.querySelector('title');
