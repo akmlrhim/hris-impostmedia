9499d54e028f007e3ec4b83efd42b73049c4e80f
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2026-05-26 15:11:39 +0800

    refactor: streamline notification classes by removing unused queueable interface

--- app/Notifications/AnnouncementPublishedNotification.php
+++ app/Notifications/AnnouncementPublishedNotification.php
@@ -3,35 +3,31 @@
 namespace App\Notifications;
 
 use App\Models\Announcement;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class AnnouncementPublishedNotification extends Notification implements ShouldQueue
+class AnnouncementPublishedNotification extends Notification
 {
-    use Queueable;
+	public function __construct(public readonly Announcement $announcement) {}
 
-    public function __construct(public readonly Announcement $announcement) {}
+	/** @return string[] */
+	public function via(object $notifiable): array
+	{
+		return ['mail'];
+	}
 
-    /** @return string[] */
-    public function via(object $notifiable): array
-    {
-        return ['mail'];
-    }
+	public function toMail(object $notifiable): MailMessage
+	{
+		$excerpt = strip_tags($this->announcement->content);
+		$excerpt = mb_strlen($excerpt) > 200 ? mb_substr($excerpt, 0, 200) . '…' : $excerpt;
 
-    public function toMail(object $notifiable): MailMessage
-    {
-        $excerpt = strip_tags($this->announcement->content);
-        $excerpt = mb_strlen($excerpt) > 200 ? mb_substr($excerpt, 0, 200).'…' : $excerpt;
-
-        return (new MailMessage)
-            ->subject('📢 Pengumuman: '.$this->announcement->title)
-            ->greeting('Halo, '.$notifiable->name.'!')
-            ->line('Ada pengumuman baru dari manajemen.')
-            ->line('**'.$this->announcement->title.'**')
-            ->line($excerpt)
-            ->action('Buka Aplikasi HRIS', route('mobile.home'))
-            ->line('Terima kasih telah menggunakan '.config('app.name').'.');
-    }
+		return (new MailMessage)
+			->subject('📢 Pengumuman: ' . $this->announcement->title)
+			->greeting('Halo, ' . $notifiable->name . '!')
+			->line('Ada pengumuman baru dari manajemen.')
+			->line('**' . $this->announcement->title . '**')
+			->line($excerpt)
+			->action('Buka Aplikasi HRIS', route('mobile.home'))
+			->line('Terima kasih telah menggunakan ' . config('app.name') . '.');
+	}
 }
--- app/Notifications/HolidayAddedNotification.php
+++ app/Notifications/HolidayAddedNotification.php
@@ -3,37 +3,33 @@
 namespace App\Notifications;
 
 use App\Models\Holiday;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class HolidayAddedNotification extends Notification implements ShouldQueue
+class HolidayAddedNotification extends Notification
 {
-    use Queueable;
+	public function __construct(public readonly Holiday $holiday) {}
 
-    public function __construct(public readonly Holiday $holiday) {}
+	/** @return string[] */
+	public function via(object $notifiable): array
+	{
+		return ['mail'];
+	}
 
-    /** @return string[] */
-    public function via(object $notifiable): array
-    {
-        return ['mail'];
-    }
+	public function toMail(object $notifiable): MailMessage
+	{
+		$message = (new MailMessage)
+			->subject('🎉 Hari Libur: ' . $this->holiday->name)
+			->greeting('Halo, ' . $notifiable->name . '!')
+			->line('Terdapat hari libur yang akan datang.')
+			->line('**' . $this->holiday->name . '**')
+			->line('📅 Tanggal: ' . $this->holiday->date->translatedFormat('l, d F Y'));
 
-    public function toMail(object $notifiable): MailMessage
-    {
-        $message = (new MailMessage)
-            ->subject('🎉 Hari Libur: '.$this->holiday->name)
-            ->greeting('Halo, '.$notifiable->name.'!')
-            ->line('Terdapat hari libur yang akan datang.')
-            ->line('**'.$this->holiday->name.'**')
-            ->line('📅 Tanggal: '.$this->holiday->date->translatedFormat('l, d F Y'));
+		if ($this->holiday->description) {
+			$message->line($this->holiday->description);
+		}
 
-        if ($this->holiday->description) {
-            $message->line($this->holiday->description);
-        }
-
-        return $message
-            ->line('Terima kasih telah menggunakan '.config('app.name').'.');
-    }
+		return $message
+			->line('Terima kasih telah menggunakan ' . config('app.name') . '.');
+	}
 }
